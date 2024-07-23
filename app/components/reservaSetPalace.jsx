13c4136d285525d168cc1edf63f0b61125bbf2cb
aldo <you@example.com>
2024-07-23 00:02:44 -0300

    selectHours

--- app/components/reservaSetPalace.jsx
+++ app/components/reservaSetPalace.jsx
@@ -1,36 +1,78 @@
 "use client";
 import Image from "next/image";
-import { useState } from "react";
+import { useState , useEffect} from "react";
 import Link from "next/link";
 import Calendar from "react-calendar";
 import "react-calendar/dist/Calendar.css";
 import { format, set } from 'date-fns'
 import { es } from 'date-fns/locale';
-import Dropdown from "./Dropdown";
-import HoursBox from "./HoursBox";
 
 export default function BookingSetPalace() {
   const [booking, setBooking] = useState(false);
   const [date, setDate] = useState(null);
   const [screenDate, setScreenDate] = useState(null)
   const [hours, setHours] = useState(null)
-  const [screenStartTime, setScreenStartTime] = useState(null)
-  const [screenEndTime, setScreenEndTime] = useState(null)
- // console.log("día: ", date);
- //console.log("hora: ", hours)
-  //console.log("screenStartTime: ", screenStartTime)
-  //console.log("screenEndTime: ", screenEndTime)
+  const [blockedTimes, setBlockedTimes] = useState([]);
+  const [start, setStart] = useState(null);
+  const [end, setEnd] = useState(null);
+  const [value, setValue] = useState('Seleccionar opción');
+  const [isOpen, setIsOpen] = useState(false);
+  const [eventContent, setEventContent] = useState(false)
+
+  useEffect(() => {
+    const getBlockedTimes = async () => {
+      try {
+        const response = await fetch('/blockedTimes.json'); // Ruta relativa al archivo en la carpeta public
+        if (!response.ok) {
+          throw new Error('Network response was not ok');
+        }
+        const data = await response.json();
+        setBlockedTimes(data.blockedTimes); // Asumiendo que el JSON tiene una estructura { "blockedTimes": [10, 11, 14] }
+    } catch (error) {
+        console.error("Error fetching blocked times:", error);
+    }
+};
+getBlockedTimes();
+}, []);
+
+const time = Array.from({ length: 15 }, (_, i) => 8 + i);
+  
+const isBlocked = (hour) => blockedTimes.includes(hour);
+const isSelected = (hour) => hour === start || hour === end || (start && end && hour > start && hour < end);
+
+const handleClick = (hour) => {
+  if (isBlocked(hour)) return;
+
+  if (start === null || (start !== null && end !== null)) {
+    setStart(hour);
+    setEnd(null);
     
+
+  } else if(start === hour){
+   setStart(null)   
+   setEnd(null)
+  } else if (hour < start) {
+    setStart(hour);
+  } else if (isValidSelection(start, hour)) {
+    setEnd(hour);
+  } };
+
+  const isValidSelection = (start, end) => {
+    if (start === null || end === null) return true;
+    for (let i = start + 1; i < end; i++) {
+      if (isBlocked(i)) return false;
+    }
+    return true;
+  };
+
+
+
   const formatToDate = (e)=>{
     const formattedDate = format(e, 'yyyy-MM-dd')
     const dateToScreen = format(e, 'PPP', { locale: es } )
       setDate(formattedDate)
       setScreenDate(dateToScreen)
    }
-    
-    const hoursSelected = (value) => {
-      setHours(value);
-    };
 
   const tileDisabled = ({ date, view }) => {
     // Este bloquea
@@ -60,14 +102,13 @@ export default function BookingSetPalace() {
   return 'custom-day';
 }
 
-const handleTimeSelection = (time) => {
-  if (!screenStartTime || (screenStartTime && screenEndTime)) {
-    setScreenStartTime(time);
-    setScreenEndTime(null);
-  } else {
-    setScreenEndTime(time);
-  }
-};
+const options = Array.from({ length: 11,}, (_, i) => `${i + 2}`+ ` hs`);
+  
+const handleSelect = (option)=>{
+  setIsOpen(!isOpen)
+  setHours(option)
+  setValue(option)
+}
 
   const Reserva = ()=>{
     if(booking === true && date=== null){
@@ -83,21 +124,119 @@ const handleTimeSelection = (time) => {
      <div className="selectHoursHero">
       <div className="selectHoursContainer">
       <p className="selectHours">{`¿Cuánto tiempo te llevará\nobtener el resultado que\ndeseas?`}</p>
-      <Dropdown onSelectChange={hoursSelected} />
+      <div className="dropdown">
+      <div className="selectedOption" onClick={() =>setIsOpen(!isOpen)}   >
+        {value}
+      </div>
+     {isOpen && (
+        <div className="options">
+          <div className="option">
+        </div>
+          {options.map((option, index) => (
+            <div
+              key={index}
+              className="option"
+              onClick={()=>handleSelect(option)}
+            >
+              {option}
+            </div>
+          ))}
+        </div>
+      )}
+    </div>
       </div>
       <div>
-      { hours && <HoursBox onClick={handleTimeSelection} /> }
+      { hours && <div>
+      {time.map((hour) => (
+        <button
+          key={hour}
+          onClick={() => handleClick(hour)}
+          className="hourBoxBtn"
+          style={{
+            backgroundColor: isBlocked(hour) ? 'grey' : isSelected(hour) ? 'cyan' : 'white',
+            color: isBlocked(hour) ? 'white' : 'black'
+          }}
+          disabled={isBlocked(hour)}
+        >
+          {hour}:00
+        </button>
+      ))}
+      <button  style={{display: end === null && "none"} } >Siguiente</button>
+    </div> }
       </div>
      </div>
-    )}
+    )} else if (eventContent) {
+      console.log("Reserva");
+    //   <div className="booking-form">
+    //   <h2>SET PALACE</h2>
+    //   <h3>31 DE JULIO 2024</h3>
+    //   <h3>14:00 - 16:00</h3>
+    //   <form onSubmit={handleSubmit}>
+    //     <div className="form-group">
+    //       <label htmlFor="name">NOMBRE Y APELLIDO:</label>
+    //       <input
+    //         type="text"
+    //         id="name"
+    //         name="name"
+    //         value={formData.name}
+    //         onChange={handleChange}
+    //         required
+    //       />
+    //     </div>
+    //     <div className="form-group">
+    //       <label htmlFor="email">MAIL:</label>
+    //       <input
+    //         type="email"
+    //         id="email"
+    //         name="email"
+    //         value={formData.email}
+    //         onChange={handleChange}
+    //         required
+    //       />
+    //     </div>
+    //     <div className="form-group">
+    //       <label htmlFor="phone">TELEFONO:</label>
+    //       <input
+    //         type="tel"
+    //         id="phone"
+    //         name="phone"
+    //         value={formData.phone}
+    //         onChange={handleChange}
+    //         required
+    //       />
+    //     </div>
+    //     <div className="form-group">
+    //       <label htmlFor="eventType">
+    //         Qué tipo de contenido quieres generar en nuestros espacios?:
+    //       </label>
+    //       <select
+    //         id="eventType"
+    //         name="eventType"
+    //         value={formData.eventType}
+    //         onChange={handleChange}
+    //         required
+    //       >
+    //         <option value="">Seleccionar...</option>
+    //         <option value="photo-session">Photo session</option>
+    //         <option value="video-shoot">Video Shoot</option>
+    //         <option value="workshop">Workshop</option>
+    //         <option value="event">Event</option>
+    //         <option value="other">Other</option>
+    //       </select>
+    //     </div>
+    //     <button type="submit">CONFIRMAR RESERVA</button>
+    //   </form>
+    // </div>
+    }
   }
   
-  
-
   const clearBooking = ()=>{
     setBooking(false)
     setDate(null)
     setHours("")
+    setStart(null)
+    setEnd(null)
+    setValue('Seleccionar opción')
   }
 
   return (
@@ -134,12 +273,14 @@ const handleTimeSelection = (time) => {
               </div>
     :
     <div className="setPalaceDetails">
-      <p className="summaryTitle">SET PALACE</p>
-      <p className="summaryDate">{screenDate && screenDate}</p>
-      <p className="summaryDate">{screenStartTime && screenStartTime}</p>
-      <p className="summaryDate">{screenEndTime && screenEndTime}</p>
-    </div>
-    
+        <p className="summaryTitle">SET PALACE</p>
+        <p className="summaryDate">{screenDate && screenDate}</p>
+        <p className="summaryDate">{hours && hours}</p>
+        <p className="summaryDate">{start && `${start}:00`}</p>
+        <p className="summaryDate">{end && `${end}:00`}</p>
+        <button onClick={() => clearBooking()} className="cancelBookingBtn"> Cancelar</button> 
+       
+      </div>
   }
       
       <div className="setPalaceDetailsContent bookingCalendarContainer">
