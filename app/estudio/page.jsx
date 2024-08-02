7f19e465692a9896b7737b50e5a6f85797dcb6cf
aldo <you@example.com>
2024-08-01 21:26:26 -0300

    videosOpt

--- app/estudio/page.jsx
+++ app/estudio/page.jsx
@@ -12,11 +12,11 @@ export default function Estudio () {
     return (
         <div>
         <div className="imgContainer">
-        <Image src={"/estudioImg.jpg"} alt="Estudio" fill className="estudioImg"/>
+        <Image src={"/estudios/estudiosImg4.jpg"} alt="Estudio" fill className="estudioImg"/>
       </div>
       <div className="handleClickSetEstudioContainer">
         <button onClick={()=>setDisplayStudio('set')} className="setEstudioBtn" style={displayEstudio === 'set' ? {backgroundColor: "black", color: "white"} : {backgroundColor: 'white', color: "black"}}>SET</button>
-        <Image src={"/camara_icon.png"} alt="camera" width={30} height={43.5} className="cameraImg" />
+        <Image src={"/minicam.png"} alt="camera" width={30} height={43.5} className="cameraImg" />
         <button onClick={()=>setDisplayStudio('equipos')} className="setEstudioBtn" style={displayEstudio === 'equipos' ? {backgroundColor: "black", color: "white"} : {backgroundColor: 'white', color: "black"}}>EQUIPOS</button>
       </div>
         {displayEstudio === 'set' ? 
